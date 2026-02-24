fef84bec363a2b7a271648e8739805b34283ca1c
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-02-24 11:48:21 +0530

    updated API base

--- worldcup_frontend/src/services/api.js
+++ worldcup_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://t20worldcup-2024-analysis-1.onrender.com/",
 });
 
 export const fetchMatches = (params) => API.get("/matches", { params });
